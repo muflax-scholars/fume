d2e298b1b4bcd8399e6379908e1d96ed66001559
muflax <mail@muflax.com>
2011-10-18 10:05:53 +0200

    added simple group to DSL

--- lib/fume/dsl.rb
+++ lib/fume/dsl.rb
@@ -11,5 +11,10 @@ module Fume
       ctx.instance_eval(&block)
       @fumes.add_context ctx
     end
+
+    def group(name, params={}, &block)
+      raise ArgumentError.new("#group requires a block") unless block_given?
+      instance_eval(&block)
+    end
   end
 end
